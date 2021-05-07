a32b52a5aed3824332d715da2fe2b9376e09e5b2
giovanni-shibaki <giovanni.shibaki@usp.br>
2021-05-07 09:36:27 -0300

    Added structs and typedefs for vehicles and bus lines

--- registry/linha.c
+++ registry/linha.c
@@ -1,4 +1,33 @@
-/*
-    Giovanni Shibaki Camargo - 11796444
-    Pedro Kenzo Muramatsu Carmo - 11796451
-*/
+/**
+ * @file linha.c
+ * @author 11796444 - Giovanni Shibaki Camargo 
+ * @author 11796451 - Pedro Kenzo Muramatsu Carmo
+ * @version 0.1
+ * @date 2021-05-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+struct _cabecalhoLinha
+{
+	char status;
+	long byteProxReg;
+	int nroRegistros;
+	int nroRegRemovidos;
+	char descreveCodigo[15];
+	char descreveCartao[13];
+	char descreveNome[13];
+	char descreveLinha[24];
+};
+
+struct _linha
+{
+	char removido;
+	int tamanhoRegistro;
+	int codLinha;
+	char aceitaCartao;
+	int tamanhoNome;
+	int tamanhoCor;
+	char* corLinha;
+};
--- registry/linha.h
+++ registry/linha.h
@@ -1,4 +1,22 @@
-/*
-    Giovanni Shibaki Camargo - 11796444
-    Pedro Kenzo Muramatsu Carmo - 11796451
-*/
+/**
+ * @file linha.h
+ * @author 11796444 - Giovanni Shibaki Camargo 
+ * @author 11796451 - Pedro Kenzo Muramatsu Carmo
+ * @version 0.1
+ * @date 2021-05-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef LINHA_H
+#define LINHA_H
+
+    #define TRUE 1
+    #define FALSE 0
+    #define boolean int
+    #define FILE_MODE "rb"
+
+    typedef struct _cabecalhoLinha CABECALHOL;
+    typedef struct _linha LINHA;
+
+#endif
