ef251fc1735b9d003863eaa12e41b90ba2099e23
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-05-07 09:50:57 -0300

    setting main 2.0

--- utils/fileManager.h
+++ utils/fileManager.h
@@ -1,4 +1,27 @@
-/*
-    Giovanni Shibaki Camargo - 11796444
-    Pedro Kenzo Muramatsu Carmo - 11796451
-*/
+/**
+ * @file fileManager.h
+ * @author 11796444 - Giovanni Shibaki Camargo 
+ * @author 11796451 - Pedro Kenzo Muramatsu Carmo
+ * @version 0.1
+ * @date 2021-05-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef FILEMANAGER_H
+    #define FILEMANAGER_H
+
+    #include <stdio.h>
+    #include <ctype.h>
+    #include <math.h>
+
+    #define TRUE 1
+    #define FALSE 0
+    #define boolean int
+    #define FILE_MODE1 "rb"
+    #define FILE_MODE2 "r"
+
+    char *readLine(FILE *stream);
+    void binarioNaTela(char *nomeArquivoBinario);
+    
+#endif
