f06196cdde5ffeaa099d79f254c55f487fb41bab
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2021-06-12 14:14:11 -0300

    adding indice to registry/

---
+++ registry/indice.c
@@ -0,0 +1,10 @@
+/**
+ * @file indice.c
+ * @author 11796444 - Giovanni Shibaki Camargo 
+ * @author 11796451 - Pedro Kenzo Muramatsu Carmo
+ * @version 0.1
+ * @date 2021-06-12
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
